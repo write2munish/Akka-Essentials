f0a288687fc46c6ecac1598ec87d742c447530d4
Munish K Gupta <write2munish@gmail.com>
2012-07-28 19:07:14 +0530

    Updated dependencies for Akka 2.0.2 and other code clean up

--- DurableMailBox/src/main/java/org/akka/essentails/durable/mailbox/example/DurableMailBox.java
+++ DurableMailBox/src/main/java/org/akka/essentails/durable/mailbox/example/DurableMailBox.java
@@ -4,41 +4,35 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
-import akka.kernel.Bootable;
 
 import com.typesafe.config.ConfigFactory;
 
-public class DurableMailBox extends UntypedActor implements Bootable{
+public class DurableMailBox extends UntypedActor {
 
 	/**
 	 * @param args
+	 * @throws Exception
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		ActorSystem system = ActorSystem.create("DurableMailBox", ConfigFactory
 				.load().getConfig("myapp1"));
-		
-		ActorRef actor = system.actorOf(new Props(DurableMailBox.class).withDispatcher("my-dispatcher"),
-				"serverActor");
-		
+
+		ActorRef actor = system
+				.actorOf(new Props(DurableMailBox.class)
+						.withDispatcher("my-dispatcher"), "serverActor");
+
 		actor.tell("Hello");
+
+		Thread.sleep(500);
+
+		system.shutdown();
 
 	}
 
 	@Override
 	public void onReceive(Object message) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void shutdown() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void startup() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Received message " + message);
 	}
 
 }
--- DurableMailBox/src/main/scala/org/akka/essentials/durable/mailbox/example/DurableMailBox.scala
+++ DurableMailBox/src/main/scala/org/akka/essentials/durable/mailbox/example/DurableMailBox.scala
@@ -1,23 +1,30 @@
 package org.akka.essentials.durable.mailbox.example
-import akka.actor.ActorSystem
-import akka.actor.Actor
-import akka.actor.Props
 import com.typesafe.config.ConfigFactory
+
+import akka.actor.actorRef2Scala
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
 
 object DurableMailBox {
 
   class DurableMailBox extends Actor {
     def receive: Receive = {
-      case _ =>
+      case message:String => System.out.println("Received Message " + message)
     }
   }
-  
-  def main(args: Array[String]): Unit = {}
-  
-  val system = ActorSystem("DurableMailBox", ConfigFactory.load()
-    .getConfig("myapp1"))
-    
-  val actor = system.actorOf(Props[DurableMailBox].withDispatcher("my-dispatcher"), name = "serverActor")
 
-  actor ! "Hello"
+  def main(args: Array[String]): Unit = {
+
+    val system = ActorSystem("DurableMailBox", ConfigFactory.load()
+      .getConfig("myapp1"))
+
+    val actor = system.actorOf(Props[DurableMailBox].withDispatcher("my-dispatcher"), name = "serverActor")
+
+    actor ! "Hello"
+    
+    Thread.sleep(500)
+    
+    system.shutdown
+  }
 }
