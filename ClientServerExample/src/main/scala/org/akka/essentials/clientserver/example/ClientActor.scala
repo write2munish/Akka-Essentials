a28431fc81e00aca3c40ad42543b59521c78899a
Munish K Gupta <write2munish@gmail.com>
2012-08-01 21:06:30 +0530

    Updated code for correctness of usage

--- ClientServerExample/src/main/scala/org/akka/essentials/clientserver/example/ClientActor.scala
+++ ClientServerExample/src/main/scala/org/akka/essentials/clientserver/example/ClientActor.scala
@@ -20,7 +20,7 @@ class ClientActor extends Actor with ActorLogging {
   var serverActor = context.actorFor("akka://ServerSys@127.0.0.1:2552/user/serverActor")
   // Second option - create remote Actor instance
   val addr = Address("akka", "ServerSys", "127.0.0.1", 2552);
-  
+
   serverActor = context.actorOf(Props[ServerActor].withDeploy(Deploy(scope = RemoteScope(addr))))
   //Third option - creating the actor using the actor name 
   //defined via application.conf
@@ -29,8 +29,8 @@ class ClientActor extends Actor with ActorLogging {
   def receive: Receive = {
     case message: String =>
       implicit val timeout = Timeout(5 seconds)
-      val future = serverActor ? message
-      val result = Await.result(future, timeout.duration).asInstanceOf[String]
-      log.info("Message received from Server -> " + result)
+      val future = (serverActor ? message).mapTo[String]
+      val result = Await.result(future, timeout.duration)
+      log.info("Message received from Server -> {}", result)
   }
 }
