e949c16edcbc4112c7a5df6bd5abd575e59a14b5
Linker Lin <linker.lin@live.com>
2014-04-16 15:04:24 +0800

    run scala-class instead of java-class

---
+++ AkkaDispatcherExample/src/main/scala/org/akka/essentials/scala/dispatcher/MsgEchoActor.scala
@@ -0,0 +1,14 @@
+package org.akka.essentials.scala.dispatcher
+import akka.actor.Actor
+
+class MsgEchoActor extends Actor {
+	var messageProcessed:Int = 0
+
+	def receive: Receive = {
+		case message =>
+			messageProcessed = messageProcessed + 1
+			println(
+				"Received Message %s in Actor %s using Thread %s, total message processed %s".format( message,
+				self.path.name, Thread.currentThread().getName(), messageProcessed))
+	}
+}
