e94c80596c47c45cbd14ea76d60e20c215e0aa95
Linker Lin <linker.lin@live.com>
2014-04-21 16:35:51 +0800

    run scala-classes

--- AkkaSTMExample/src/main/scala/org/akka/essentials/stm/pingpong/example2/Player.scala
+++
@@ -1,8 +0,0 @@
-package org.akka.essentials.stm.pingpong.example2
-
-class Player(myOpponent: String, myTable: PingPong) extends Runnable {
-
-  override def run(): Unit = {
-    while (myTable.hit(myOpponent)) {}
-  }
-}
